2c96724f98e66eb1fdc202ed3616a393444fc276
Na-ux315 <06naila06@gmail.com>
2025-03-20 20:57:49 +0700

    Update core_monitor.sh

--- soal_2/scripts/core_monitor.sh
+++ soal_2/scripts/core_monitor.sh
@@ -3,9 +3,7 @@
 LOG_FILE="./logs/core.log"
 TIMESTAMP=$(date "+[%Y-%m-%d %H:%M:%S]")
 
-# Ambil penggunaan CPU dalam persen
 CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 CPU_MODEL=$(lscpu | grep "Model name" | sed -E 's/Model name:\s+//')
 
-# Simpan log dengan format yang sesuai
 echo "$TIMESTAMP -- Core Usage [$(printf "%.5f" "$CPU_USAGE")%] -- Terminal Model [$CPU_MODEL]" >> "$LOG_FILE"
